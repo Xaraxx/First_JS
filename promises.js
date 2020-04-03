923668c9306862305158a7400a2ce6b964c2f2e9
sara <sara.galvan.o91@gmail.com>
2020-04-02 21:19:27 -0500

    making promises

--- promises.js
+++ promises.js
@@ -3,36 +3,48 @@ const PEOPLE_URL = 'people/:id'
 
 
 function getCharacter(id, ){
-    return new Promise(function(resolve, reject){
+    return new Promise((resolve, reject) => {
         const url =`${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    })
-
-    
-    $.get(url, {crossDomain:true}, callback).fail(function(){
-        console.log(`We have something wrong at this moment. Is impossible get the character ${id} `)
+        $.get(url, {crossDomain:true}, function(data){
+            resolve(data)
+        })
+        .fail(() => reject(id))
     })
 }
 
-getCharacter(1, function(character){
-    console.log(`Hello! I'm ${character.name}`) 
+getCharacter(1)
+
+.then(function(character){
+    console.log(`Hello! I'm ${character.name}`)
 })
 
-getCharacter(1, function(character){
-    console.log(`Hello! I'm ${character.name}`) 
-    getCharacter(2, function(character){
-        console.log(`Hello! I'm ${character.name}`) 
-        getCharacter(3, function(character){
-            console.log(`Hello! I'm ${character.name}`)
-            getCharacter(4, function(character){
-                console.log(`Hello! I'm ${character.name}`) 
-                getCharacter(5, function(character){
-                    console.log(`Hello! I'm ${character.name}`) 
-                    getCharacter(6, function(character){
-                        console.log(`Hello! I'm ${character.name}`)
-                    })
-                })
-            })
-        })
-    })
-})
+.catch(onError)
+
+function onError(id){
+    console.log(`We have something wrong at this moment. Is impossible get the character ${id} `)}
+
+
+
+// getCharacter(1, function(character){
+//     console.log(`Hello! I'm ${character.name}`) 
+// })
+
+// getCharacter(1, function(character){
+//     console.log(`Hello! I'm ${character.name}`) 
+//     getCharacter(2, function(character){
+//         console.log(`Hello! I'm ${character.name}`) 
+//         getCharacter(3, function(character){
+//             console.log(`Hello! I'm ${character.name}`)
+//             getCharacter(4, function(character){
+//                 console.log(`Hello! I'm ${character.name}`) 
+//                 getCharacter(5, function(character){
+//                     console.log(`Hello! I'm ${character.name}`) 
+//                     getCharacter(6, function(character){
+//                         console.log(`Hello! I'm ${character.name}`)
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// })
 
