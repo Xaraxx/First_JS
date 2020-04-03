7b3aeea902b1b4aafd7d26e020013bb63712be91
sara <sara.galvan.o91@gmail.com>
2020-04-02 20:52:26 -0500

    hadling errors and starting with promises

---
+++ errosInCallback.js
@@ -0,0 +1,34 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+
+
+function getCharacter(id, callback){
+    const url =`${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, {crossDomain:true}, callback).fail(function(){
+        console.log(`We have something wrong at this moment. Is impossible get the character ${id} `)
+    })
+}
+
+getCharacter(1, function(character){
+    console.log(`Hello! I'm ${character.name}`) 
+})
+
+getCharacter(1, function(character){
+    console.log(`Hello! I'm ${character.name}`) 
+    getCharacter(2, function(character){
+        console.log(`Hello! I'm ${character.name}`) 
+        getCharacter(3, function(character){
+            console.log(`Hello! I'm ${character.name}`)
+            getCharacter(4, function(character){
+                console.log(`Hello! I'm ${character.name}`) 
+                getCharacter(5, function(character){
+                    console.log(`Hello! I'm ${character.name}`) 
+                    getCharacter(6, function(character){
+                        console.log(`Hello! I'm ${character.name}`)
+                    })
+                })
+            })
+        })
+    })
+})
+
