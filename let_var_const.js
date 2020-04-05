8ce657638e6103afc7d0c3a27e2d2db264ba8db0
sara <sara.galvan.o91@gmail.com>
2020-04-04 23:20:16 -0500

    learning the memoization concept

--- let_var_const.js
+++ let_var_const.js
@@ -29,6 +29,24 @@ function integerDivision(dividend, divisor){
     if (dividend < divisor){ 
         return 0
     }
-    
+
     return 1 + integerDivision(dividend - divisor, divisor)
+}
+
+function factorial(n){
+    if (!this.cache){
+        this.cache = {}
+    }
+    debugger
+    if (this.cache[n]){
+        return this.cache[n]
+    }
+
+    if(n === 1){
+        return 1
+    }
+
+    this.cache[n] = n * factorial(n - 1)
+    debugger   
+    return this.cache[n]
 }
